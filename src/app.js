421f9b271b9d9ebc2587e542c29e702ebd71a009
Saksham Pardesi <100712207+Sakshampardesi5831@users.noreply.github.com>
2026-02-24 21:36:12 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express, { json } from "express";
+import { wrapRoutes } from "./routes/wrap-routes";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config()
+const app = express();
+
+
+
+app.use(cors());
+app.use(json());
+const PORT = process.env.PORT || 3000;
+wrapRoutes(app);
+
+app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
